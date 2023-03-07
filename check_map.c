d3bc2cb613f1f320f4c11f46081dbfaee98996a8
Mathis Ravera <mravera@student.42lausanne.ch>
2023-03-07 19:28:56 +0100

    deplacement de fonction

--- check_map.c
+++ check_map.c
@@ -6,7 +6,7 @@
 /*   By: mravera <mravera@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 14:50:06 by mravera           #+#    #+#             */
-/*   Updated: 2023/03/07 16:30:07 by mravera          ###   ########.fr       */
+/*   Updated: 2023/03/07 17:13:01 by mravera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,42 +32,9 @@ int	check_map(t_data *data)
 	}
 	if (x >= 0 && x != 1)
 		return (print_nsew(x));
-/*
+	/*
 	if (is_map_closed(m))
 		return (1);
 	*/
-	return (0);
-}
-
-int	check_strmap(char *str)
-{
-	int	x;
-	int	i;
-
-	i = 0;
-	x = 0;
-	while (str[i])
-	{
-		if (!is_mapkey(str[i]))
-			return (-1);
-		if (str[i] == 'N' || str[i] == 'S' || str[i] == 'E' || str[i] == 'W')
-			x ++;
-		i ++;
-	}
-	return (x);
-}
-
-int	print_maperr(void)
-{
-	printf("\nError\nWrong char in map.\n");
-	return (-1);
-}
-
-int	print_nsew(int x)
-{
-	if (x == 0)
-		printf("Error\nNo NSEW char in map.\n");
-	else
-		printf("Error\nToo many NSEW char in map [%d]\n", x);
 	return (0);
 }
--- map_utils.c
+++ map_utils.c
@@ -6,7 +6,7 @@
 /*   By: mravera <mravera@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 16:30:27 by mravera           #+#    #+#             */
-/*   Updated: 2023/03/03 18:05:15 by mravera          ###   ########.fr       */
+/*   Updated: 2023/03/07 17:09:48 by mravera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,37 @@ int	is_mapkey(char c)
 		return (1);
 	else
 		return (0);
+}
+
+int	check_strmap(char *str)
+{
+	int	x;
+	int	i;
+
+	i = 0;
+	x = 0;
+	while (str[i])
+	{
+		if (!is_mapkey(str[i]))
+			return (-1);
+		if (str[i] == 'N' || str[i] == 'S' || str[i] == 'E' || str[i] == 'W')
+			x ++;
+		i ++;
+	}
+	return (x);
+}
+
+int	print_maperr(void)
+{
+	printf("\nError\nWrong char in map.\n");
+	return (-1);
+}
+
+int	print_nsew(int x)
+{
+	if (x == 0)
+		printf("Error\nNo NSEW char in map.\n");
+	else
+		printf("Error\nToo many NSEW char in map [%d]\n", x);
+	return (0);
 }
